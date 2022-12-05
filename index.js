105d225576c34f1960954e669dd1d8740f6834e2
jawidh-muhammadh <jawidhmuhammadh@gmail.com>
2022-12-05 17:14:52 +0530

    new commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const PORT = process.env.PORT || 3000;
 
 app.get( "" , (req,res) => {
-    res.send("hello this is server")
+    res.send("hello this is server,How are you")
 } );
 
 
